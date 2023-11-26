35b31ceebd8d999e064a3eb23ac30d95ca04aa73
agostinoluca <agostino.luca89@gmail.com>
2023-11-26 15:09:50 +0100

    add track of the exercise add operations in italian

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-console.log('Hello World');
+/* 
+    #Mail
+        Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+    #Gioco dei dadi
+        Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+    ##Tools:
+    -console.log (metodi);
+    -let / const (variabili);
+    -prompt / alert (funzioni);
+    -if / if else / else (condizioni);
+    -< > (operatori);
+    -array (oggetto).
+*/
+
+
+// tramite un prompt chiedo all'utente di inserire la propria mail
+// controllo se la mail inserita è presente nella lista con le mail autorizzate ad accedere
+    // se è presente nella lista presento un alert di benvenuto e inizializzo tramite prompt il gioco
+        // tramite un prompt chiedo all'utente di lanciare i dadi o di annullare l'operazione
+            // se accetta di giocare
+                // genero un numero casuale per l'utente
+                // genero un numero casuale per il computer
+                // paragono i risultati casuali e definisco il vincitore
+                // stampo in pagina il vincitore
+            // se non accetta di giocare 
+                // saluto l'utente tramite un alert
+    // se non è nella lista invio un alert con l'avviso di respingimento
